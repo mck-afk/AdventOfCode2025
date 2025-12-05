de5698b52ab5928140c823e180dddd075efbc185
annabelle <annabelle@gmail.com>
2025-12-05 14:16:07 +1100

    part 1 complete

--- day2.py
+++ day2.py
@@ -20,26 +20,30 @@ if __name__ == "__main__":
 print(f"instructs {instructions[0]}")
 
 invalidIDtotal = 0
+sample = instructions[1:2]
     
 #for each item in instructions
 for item in instructions:
     print(item)
     length = len(item['end'])
-    if (length == 2) or (length == 4) or (length == 6) or (length == 8) or (length == 10): #even means there can be two halves
-        midPoint = int(length/2)
-        print(midPoint)
-        #create counter
-        counter = item['start']
-        #start counting and checking
-        while counter <= item['end']:
-            counter = int(counter) + 1
+    #if (len(item['start']) % 2 == 0) or (len(item['end']) % 2 == 0):
+    #if (length == 2) or (length == 4) or (length == 6) or (length == 8) or (length == 10): #even means there can be two halves
+    midPoint = int(length/2)
+    print(midPoint)
+    #create counter
+    counter = item['start']
+    #start counting and checking
+    
+    while int(counter) <= int(item['end']):
+        counter = int(counter) + 1
+        counter = str(counter)
+        if (counter[0:midPoint] == counter[midPoint:]):
+            counter = int(counter)
+            invalidIDtotal += counter
             counter = str(counter)
-            if (counter[0:midPoint] == counter[midPoint:]):
-                counter = int(counter)
-                invalidIDtotal += counter
-                counter = str(counter)
-                print(f"matches omg {counter}")
+            print(f"matches omg {counter}")
 
 print(invalidIDtotal)
 
-# Part 1 asnwer: > 19541758938
+# Part 1 asnwer: 19605500130
+
